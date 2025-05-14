26652ecc2acdab3c1b6c13ec55f3ee0015bdf9ad
Cristian <cristianrodriguezlugo@gmail.com>
2025-05-14 08:38:24 +0200

    avance

--- 01-fundamentos/assetts/js/alerts.js
+++ 01-fundamentos/assetts/js/alerts.js
@@ -1,1 +1,6 @@
-console.log(miNombre);
+
+// // alert ('hola mundo');
+
+// // let nombre = prompt('¿Cuál es tu nombre?', 'Escribe tu nombre');
+
+// const seleccion= confirm('¿Está seguro de que desea continuar?');
