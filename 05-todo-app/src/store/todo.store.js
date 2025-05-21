77cac8a763487901874360c008444a8aca971480
USUARIO\crist <e>
2025-05-21 23:33:58 +0200

    añadido funciones de la todo app

--- 05-todo-app/src/store/todo.store.js
+++ 05-todo-app/src/store/todo.store.js
@@ -11,6 +11,9 @@ const state = {
     new Todo("Pieda del alma"),
     new Todo("Pieda del infinito"),
     new Todo("Pieda del tiempo"),
+    new Todo("Pieda del poder"),
+    new Todo("Pieda de la realidad"),
+
   ],
   filter: Filters.All,
 };
@@ -20,6 +23,66 @@ const initStore = () => {
   console.log("InitStore 🥑🥑🥑");
 };
 
+const loadStore = () => {
+  throw new Error("Not implemented");
+};
+
+const getTodos = (filter = Filters.All) => {
+  switch (filter) {
+    case Filters.All:
+      return [...state.todos];
+    case Filters.Completed:
+      return state.todos.filter((todo) => todo.done);
+    case Filters.Pending:
+      return state.todos.filter((todo) => !todo.done);
+    default:
+      throw new Error(`Option ${filter} is not valid`);
+      break;
+  }
+};
+
+const toggleTodo = (todoId) => {
+  
+  state.todos = state.todos.map(todo =>{
+      if (todo.id === todoId) {
+        todo.done = !todo.done;
+      }
+      return todo;
+  })
+};
+
+const addTodo = (description) => {
+  if (!description) throw new Error("Description is required");
+  state.todos.push(new Todo(description));
+};
+
+const deleteTodo = (todoId) => {
+  state.todos = state.todos.filter((todo) => todo.id !== todoId);
+};
+
+const deleteCompleted = () => {
+  state.todos = state.todos.filter((todo) => !todo.done);
+};
+
+const setFilter = (newFilter = Filters.All) => {
+  if (!Object.values(Filters).includes(newFilter)) {
+    throw new Error(`Filter ${newFilter} is not valid`);
+  }
+  state.filter = newFilter;
+};
+
+const getCurrentFilter = () => {
+  return state.filter;
+};
+
 export default {
+  addTodo,
+  deleteCompleted,
+  deleteTodo,
+  getCurrentFilter,
+  getTodos,
   initStore,
+  loadStore,
+  setFilter,
+  toggleTodo,
 };
