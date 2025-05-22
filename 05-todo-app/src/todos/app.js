1710b474e1faaa776c60ada98793ea19a320ae0b
Cristian <cristianrodriguezlugo@gmail.com>
2025-05-22 10:23:24 +0200

    agregados listeners para agregar todos, cambiar estado y para eliminar

--- 05-todo-app/src/todos/app.js
+++ 05-todo-app/src/todos/app.js
@@ -1,4 +1,14 @@
+import todoStore from "../store/todo.store";
 import html from "./app.html?raw";
+import { renderTodos } from "./use-cases";
+
+
+//selectores css de app.html donde insertamos el custom html
+const ElementIds = {
+  TodoList: '.todo-list',
+  NewTodoInput: '#new-todo-input',
+  DestroyTodo: '.destroy'
+}
 
 /**
  * 
@@ -7,10 +17,54 @@ import html from "./app.html?raw";
 
 
 export const App = (elementId) => {
+
+
+  const displayTodos = () =>{
+    const todos = todoStore.getTodos( todoStore.getCurrentFilter());
+    renderTodos(ElementIds.TodoList,todos);
+  }
+
+
   //cuando la funcion App() se llama
   (() => {
     const app = document.createElement("div");
     app.innerHTML = html;
     document.querySelector(elementId).append(app);
+    displayTodos();
   })();
+
+
+  //referencias HTML
+  const newDescriptionInput = document.querySelector(ElementIds.NewTodoInput);
+  const todoListUl = document.querySelector(ElementIds.TodoList);
+
+  //Listeners
+
+  newDescriptionInput.addEventListener('keyup', (event) => {
+    if (event.keyCode !== 13) return;
+    if (event.target.value.trim().length===0) return;
+
+    todoStore.addTodo (event.target.value);
+    displayTodos();
+    event.target.value='';
+  });
+
+  todoListUl.addEventListener('click',(event) =>{
+    const element = event.target.closest('[data-id]')
+    todoStore.toggleTodo(element.getAttribute('data-id'));
+    displayTodos();
+  });
+
+  todoListUl.addEventListener('click',(event) =>{
+    const isDestroyElement = event.target.className === 'destroy';
+    const element = event.target.closest('[data-id]')
+    if (!element || !isDestroyElement) return;
+
+    todoStore.deleteTodo(element.getAttribute('data-id'));
+    displayTodos();
+  });
+
+
 };
+
+
