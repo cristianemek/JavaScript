e13a898fe3afc263f57ecdb25acaebc53fadbca3
USUARIO\crist <e>
2025-05-23 19:52:01 +0200

    callbacks y manejo de errores

--- 06-advanced/src/concepts/02-callbacks.js
+++ 06-advanced/src/concepts/02-callbacks.js
@@ -5,20 +5,35 @@ import { heroes } from "../data/heroes";
  * @param {HTMLDivElement} element 
  */
 export const callBacksComponent = (element) =>{
+    const id = '5d86371f2343e37870b91ef1e';
 
+    findHero(id, (error,hero) =>{
+        if(error) {
+            element.innerHTML=error;
+            return;
+        }
+
+        element.innerHTML=hero.name;
+    });
 
 }
 
 /**
  * 
  * @param {string} id 
- * @param {*} callback 
+ * @param { (error: String|Null, hero: Object) => void } callback 
  */
 
 
 const findHero = (id, callback) =>{
 
     const hero = heroes.find(hero => hero.id === id);
-    callback(hero);
+
+    if(!hero){
+        callback(`Hero with id ${id} not found.`);
+        return;
+    }
+
+    callback(null,hero);
 
 }
